cc6f8b95438a23215e7e09a307093577cc132f37
snoozyz <150150378+snoozyz@users.noreply.github.com>
2026-02-03 09:32:25 +0100

    maak inlog netter

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-try{
-    $db= new PDO("mysql:host=localhost;dbname=dms-spakaas","root","");
-}
-catch(PDOException $melding)
-{
-    echo $melding->getmessage();
-}
-?>
--- library_function.php
+++
@@ -1,9 +0,0 @@
-<?php
-function find_user($userMail, $password) {
-    include("config.php");
-    $v = $db->prepare("SELECT * FROM gebruiker WHERE email = ? AND wachtwoord = ?");
-    $v->execute(array($userMail,$password));
-    return $v->fetch(PDO::FETCH_ASSOC);
-}
-
-?>
